8d5e1c375b5226b480255b5692c3905cc9b04b83
astrhsptl <dctrback@bk.ru>
2023-07-09 23:34:00 +0300

    add burger menu

---
+++ static/pages/js/menu.js
@@ -0,0 +1,7 @@
+function openHeaderMenu() {
+    document.getElementById("burger_opened").style.display = "block";
+}
+
+function closeHeaderMenu() {
+    document.getElementById("burger_opened").style.display = "none";
+}
