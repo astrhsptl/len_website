7c702ac0104849e7c4c21e998172129cb290d2f1
astrhsptl <dctrback@bk.ru>
2023-07-10 16:11:00 +0300

    styled pk

--- pages/models.py
+++ pages/models.py
@@ -33,6 +33,7 @@ class Products(models.Model):
     city = models.CharField(max_length=255, verbose_name="Местоположение")
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, verbose_name="Пользователь")
     services = models.CharField(max_length=255, verbose_name="Услуги", choices=SERVICES)
+    image = models.FileField(upload_to='products', verbose_name='Фотография', blank=True, null=True)
 
     def __str__(self):
         return self.title
