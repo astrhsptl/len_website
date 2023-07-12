9c665a53ebada62f43c53799f018e1b4aac450e1
astrhsptl <dctrback@bk.ru>
2023-07-13 00:17:45 +0300

    one trouble: animation on burger menu

--- API/urls.py
+++ API/urls.py
@@ -4,7 +4,7 @@ from .views import ProductCreateAPIView, UserUpdateAPIView
 
 
 urlpatterns = [
-    path("product/create/", ProductCreateAPIView.as_view(), name="product_create"),
-    path("user/update/<int:pk>/", UserUpdateAPIView.as_view(), name="user_update"),
+    path("product/create/", ProductCreateAPIView.as_view(), name="product_create_api"),
+    path("user/update/<int:pk>/", UserUpdateAPIView.as_view(), name="user_update_api"),
     
 ]
--- static/pages/js/menu.js
+++ static/pages/js/menu.js
@@ -1,7 +1,13 @@
 function openHeaderMenu() {
-    document.getElementById("burger_opened").style.display = "block";
+    let menu = document.getElementById("burger_opened");
+
+    menu.classList.add('burger_state_open')
+
+    document.getElementsByTagName("body")[0].style.overflow = "hidden";
 }
 
 function closeHeaderMenu() {
-    document.getElementById("burger_opened").style.display = "none";
+    document.getElementById("burger_opened").classList.remove('burger_state_open')
+    document.getElementsByTagName("body")[0].style.overflow = "auto";
+
 }
