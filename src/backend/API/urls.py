357c3cbf26ecd261dc75fecf9a3416840600844a
astrhsptl <dctrback@bk.ru>
2024-01-12 08:21:00 +0300

    add .

--- src/backend/API/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-
-from .views import ProductCreateAPIView
-
-
-urlpatterns = [
-    path("product/create/", ProductCreateAPIView.as_view(), name="product_create_api"),
-]
--- src/backend/static/pages/js/register.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("id_avatar").oninput = function() {
-    let imgLabel = document.getElementsByTagName("label")[0]
-    let img = document.getElementById("id_avatar")
-    imgLabel.textContent = img.files[0].name 
-}
